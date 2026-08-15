69ee9b294f71846be3413054d41cf9cd95e78186
Duralumin <202533636+DuraluminHere@users.noreply.github.com>
2026-08-15 15:32:58 -0400

    Update build.cjs

--- build.cjs
+++ build.cjs
@@ -593,7 +593,7 @@ const output = `<!DOCTYPE html>
 
     .media-item img {
       width: 100%;
-      height: 100%;
+      height: 240px;
       object-fit: contain;
       background: var(--surface);
       display: block;
